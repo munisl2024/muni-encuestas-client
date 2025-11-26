fad802e18e87018cfdea0cf7466f98708d7887bf
ing.lucasmoreno <ing.lucasmoreno@gmail.com>
2025-11-26 17:42:10 -0300

    Para produccion - Remoto

--- src/environments/environments.prod.ts
+++ src/environments/environments.prod.ts
@@ -1,6 +1,6 @@
 export const environments = {
   production: true,
   // base_url: 'http://localhost:3000/api',
-  // base_url: 'http://45.178.3.232:9235/api',
-  base_url: 'http://10.15.8.10:9236/api'
+  base_url: 'http://45.178.3.232:9235/api',
+  // base_url: 'http://10.15.8.10:9236/api'
 }
